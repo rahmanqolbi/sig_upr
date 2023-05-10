01f7d4cb1e5f83737fd1541b0cb07a4b69ba405a
rahmanqolbi <98144116+rahmanqolbi@users.noreply.github.com>
2023-05-10 11:33:13 +0000

    zoom control position

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
-var map = L.map('map').setView([-2.2165470720021343, 113.89801487350582], 18);
+var map = L.map('map', {
+  zoomControl: false
+}).setView([-2.2165470720021343, 113.89801487350582], 18);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+L.control.zoom({
+  position: 'bottomright'
 }).addTo(map);
 
 L.marker([-2.2159230106564487, 113.89870865952419])
